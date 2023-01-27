5475f6a9c52a409933da3ff1cbe629750f8e6ddd
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-01-27 21:53:05 +0530

    imporved rotate array

--- Rotate Array.cpp
+++ Rotate Array.cpp
@@ -6,9 +6,8 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         k %= nums.size();
-        vector<int> ans = nums;
-        for(int i = 0;i<nums.size();i++){
-            nums[(i + k) % nums.size()] = ans[i];
-        }
+        reverse(nums.begin(), nums.end() - k);
+        reverse(nums.end() - k,nums.end());
+        reverse(nums.begin(),nums.end());
     }
 };
