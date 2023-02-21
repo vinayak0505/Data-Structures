7df2165aeba24975f738cdf3aeea44a92657fdc0
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-02-21 20:51:47 +0530

    completed Single Element in a Sorted Array from leetcode

---
+++ Single Element in a Sorted Array.cpp
@@ -0,0 +1,12 @@
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int ans = 0;
+        for(int num:nums) ans ^= num;
+        return ans;
+    }
+};
