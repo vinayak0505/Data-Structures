492d69de7017ed9d13ce8eedf87a75dc9b77c6ce
vinayak0505 <vinayakaggarwal05@gmail.com>
2026-03-13 21:24:27 +0530

    refactor isValid method and optimize counting logic in Count Subarrays With K Distinct Integers

--- Count Subarrays With K Distinct Integers.java
+++ Count Subarrays With K Distinct Integers.java
@@ -2,13 +2,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-    public boolean isValid(Map<Integer, Integer> mp, int k, int m) {
+    public boolean isValid(Map<Integer, Integer> mp, int k, int more) {
         if (mp.size() != k)
             return false;
-        for (Integer values : mp.values()) {
-            if (values < m)
-                return false;
-        }
+        if(more != k) return false;
         return true;
     }
 
@@ -16,13 +13,25 @@ class Solution {
         Map<Integer, Integer> slow = new HashMap<>();
         Map<Integer, Integer> fast = new HashMap<>();
         long vcount = 0;
+        int fastMore = 0, slowMore = 0;
 
         for (int right = 0, slowp = 0, fastp = 0; right < nums.length; right++) {
-            slow.put(nums[right], slow.getOrDefault(nums[right], 0) + 1);
-            fast.put(nums[right], fast.getOrDefault(nums[right], 0) + 1);
+            int slowValue = slow.getOrDefault(nums[right], 0) + 1;
+            if(slowValue == m){
+                slowMore++;
+            }
+            slow.put(nums[right], slowValue);
+            int fastValue = fast.getOrDefault(nums[right], 0) + 1;
+            if(fastValue == m){
+                fastMore++;
+            }
+            fast.put(nums[right], fastValue);
 
-            while ((fast.size() > k  || isValid(fast, k, m)) && fastp <= right) {
+            while ((fast.size() > k || isValid(fast, k, fastMore)) && fastp <= right) {
                 int newValue = fast.get(nums[fastp]) - 1;
+                if(newValue == m - 1){
+                    fastMore--;
+                }
                 if (newValue == 0) {
                     fast.remove(nums[fastp]);
                 } else {
@@ -33,6 +42,9 @@ class Solution {
 
             while (slow.size() > k && slowp <= right) {
                 int newValue = slow.get(nums[slowp]) - 1;
+                if(newValue == m - 1){
+                    slowMore--;
+                }
                 if (newValue == 0) {
                     slow.remove(nums[slowp]);
                 } else {
@@ -40,7 +52,7 @@ class Solution {
                 }
                 slowp++;
             }
-            if (isValid(slow, k, m)) {
+            if (isValid(slow, k, slowMore)) {
                 System.out.println(fastp - slowp);
                 vcount += (long)(fastp - slowp);
             }
