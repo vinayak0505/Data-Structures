77803c8f83bdd91b5f1d636b14f82f2259b28f6a
vinayak0505 <vinayakaggarwal05@gmail.com>
2026-05-03 22:06:33 +0530

    implement Maximize Fixed Points After Deletions solution

---
+++ 3920. Maximize Fixed Points After Deletions.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+
+    class Pair {
+        int val;
+        int diff;
+        
+        Pair(int val, int diff) {
+            this.val = val;
+            this.diff = diff;
+        }
+    }
+
+    private int upperBound(ArrayList<Integer> array, int value) {
+        int l = 0, r = array.size();
+        if (r == 0)
+            return 0;
+        int ans = r;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (array.get(mid) > value) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    private int helper(ArrayList<Integer> diff) {
+        int n = diff.size();
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int value = diff.get(i);
+            int pos = upperBound(array, value);
+            if (array.size() == pos)
+                array.add(value);
+            else
+                array.set(pos, value);
+        }
+        return array.size();
+    }
+
+    public int maxFixedPoints(int[] nums) {
+        int n = nums.length;
+        ArrayList<Pair> array = new ArrayList<>(n);
+        for (int i = 0; i < nums.length; i++) {
+            if (i >= nums[i]) {
+                array.add(new Pair(nums[i], i - nums[i]));
+            }
+        }
+
+        Collections.sort(array, (a, b) -> {
+            if(a.val == b.val){
+                return b.diff - a.diff;
+            }
+            return a.val - b.val;
+        });
+        ArrayList<Integer> validDiffs = new ArrayList<>(n);
+        for (Pair p : array) {
+            validDiffs.add(p.diff);
+        }
+        return helper(validDiffs);
+    }
+}
