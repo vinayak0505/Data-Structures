e73e1b4cbf5a6647d728e97ce707a1693efadc76
vinayak0505 <vinayakaggarwal05@gmail.com>
2026-06-21 12:32:50 +0530

    feat: replace hash map approach with segment tree for optimal range updates and queries in longest balanced subarray solution

--- 3721. Longest Balanced Subarray II.java
+++ 3721. Longest Balanced Subarray II.java
@@ -1,37 +1,114 @@
 import java.util.Arrays;
-import java.util.HashMap;
+
+class SegmentTree {
+
+    class Node {
+        public int max;
+        public int min;
+        public int lazy;
+
+        public Node() {
+            max = 0;
+            min = 0;
+            lazy = 0;
+        }
+    }
+
+    Node[] array;
+    int n;
+
+    public SegmentTree(int size) {
+        n = size;
+        array = new Node[4 * n];
+        for (int i = 0; i < 4 * n; i++) {
+            array[i] = new Node();
+        }
+    }
+
+    private void push(int node, int start, int end){
+        if(array[node].lazy == 0) return;
+        array[node].max += array[node].lazy;
+        array[node].min += array[node].lazy;
+        if(start != end){
+            array[node * 2].lazy += array[node].lazy;
+            array[node * 2 + 1].lazy += array[node].lazy;
+        }
+        array[node].lazy = 0;
+    }
+
+    private void update(int node, int value, int start, int end, int queryStart, int queryEnd){
+
+        push(node, start, end);
+
+        if(queryEnd < start || end < queryStart || end < start) return;
+
+        if(queryStart <= start && end <= queryEnd){
+            array[node].lazy += value;
+            push(node, start, end);
+            return;
+        }
+
+        int mid = start + (end - start) / 2;
+        update(2 * node, value, start, mid, queryStart, queryEnd);
+        update(2 * node + 1, value, mid + 1, end, queryStart, queryEnd);
+
+        array[node].max = Math.max(array[2 * node].max, array[2 * node + 1].max);
+        array[node].min = Math.min(array[2 * node].min, array[2 * node + 1].min);
+    }
+
+    public void update(int value, int start, int end) {
+        update(1, value, 0, n - 1, start, end);
+    }
+
+    private boolean hasZero(int node, int start, int end){
+        if(array[node].min <= 0 && 0 <= array[node].max) return true;
+        return false;
+    }
+
+    private int query(int nodeIndex, int left, int right, int leftQuery, int rightQuery){
+        push(nodeIndex, left, right);
+        if(right < leftQuery || rightQuery < left || !hasZero(nodeIndex, left, right)) return -1;
+
+        if(left == right){
+            if(hasZero(nodeIndex, left, right)){
+                return left;
+            }
+            return -1;
+        }
+
+        int mid = left + (right - left) / 2;
+        int ans = query(2 * nodeIndex, left, mid, leftQuery, rightQuery);
+        if(ans != -1) return ans;
+        return query(2 * nodeIndex + 1, mid + 1, right, leftQuery, rightQuery);
+
+    }
+
+    public int query(int end) {
+        int ans = query(1, 0, n - 1, 0, end);
+        if(ans == -1) return end + 1;
+        return ans;
+    }
+
+}
 
 class Solution {
+    int maxValue = (int) 1e5 + 1;
 
-    private int[] firstOcc;
-    
-    private void buildFirstOcc(int[] nums){
-        int max = (int) 1e5 + 1;
-        firstOcc = new int[max];
-        Arrays.fill(firstOcc, -1);
-    }
-    
     public int longestBalanced(int[] nums) {
-        buildFirstOcc(nums);
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int sum = 0;
+        int place[] = new int[maxValue];
+        Arrays.fill(place, -1);
+        SegmentTree st = new SegmentTree(nums.length);
+
         int ans = 0;
-        map.put(0, 0);
-        for(int i = 0; i< nums.length;i++){
-            if(firstOcc[nums[i]] == -1){
-                firstOcc[nums[i]] = i;
-                if(nums[i] % 2 == 0){
-                    sum--;
-                }else{
-                    sum++;
-                }
-            }
-            System.out.println(sum + " " + i);
-            if(map.containsKey(sum)){
-                ans = Math.max(ans, i - map.get(sum) + 1);
-            }else{
-                map.put(sum, i);
-            }
+        for (int r = 0; r < nums.length; r++) {
+            boolean odd = (nums[r] % 2) == 1;
+            int prev = place[nums[r]];
+            st.update(odd ? 1 : -1, prev + 1, r);
+            place[nums[r]] = r;
+
+            int l = st.query(r);
+            ans = Math.max(ans, r - l + 1);
+
         }
         return ans;
     }
