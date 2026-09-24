5eb60cb7122554e8914c3f736df5dfa9622fef08
vinayak0505 <vinayakaggarwal05@gmail.com>
2026-09-24 10:33:01 +0530

    feat: optimize numberOfSubsequences method by simplifying HashMap usage

--- 3404. Count Special Subsequences.java
+++ 3404. Count Special Subsequences.java
@@ -22,28 +22,19 @@ class Solution {
     public long numberOfSubsequences(int[] nums) {
         long ans = 0;
         int n = nums.length;
-        HashMap<String, Long> dp[] = new HashMap[n];
-        for (int j = n - 3; j >= 0; j--) {
-            if (dp[j + 1] != null) {
-                dp[j] = new HashMap<>(dp[j + 1]);
-            } else {
-                dp[j] = new HashMap<>();
+        HashMap<String, Long> dp = new HashMap<>();
+        for (int r = 4; r < n; r++) {
+            int q = r - 2;
+            for (int p = 0; p <= q - 2; p++) {
+                String g = reduceFraction(nums[p], nums[q]);
+                dp.put(g, dp.getOrDefault(g, 0l) + 1);
             }
 
-            for (int i = j + 2; i < n; i++) {
-                String key = reduceFraction(nums[i], nums[j]);
-                dp[j].put(key, dp[j].getOrDefault(key, 0L) + 1);
+            for (int s = r + 2; s < n; s++) {
+                String g = reduceFraction(nums[s], nums[r]);
+                ans += dp.getOrDefault(g, 0l);
             }
-        }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 2; j < n - 2; j++) {
-                String key = reduceFraction(nums[i], nums[j]);
-                if (dp[j + 2] == null) {
-                    continue;
-                }
-                ans += dp[j + 2].getOrDefault(key, 0l);
-            }
         }
         return ans;
     }
